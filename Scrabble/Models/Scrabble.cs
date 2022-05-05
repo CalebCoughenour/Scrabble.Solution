201ecf1d7a529eaa597cbb8c173a21670be420bf
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-05 15:39:51 -0700

    refractor logic.
    Co-authored-by: Caleb Coughenour <CalebCoughenour@users.noreply.github.com>

--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -18,35 +18,35 @@ namespace Scrabble
     public int ScoreCalc()
     {
       foreach(char Character in UserWordArray)
-      {
-          if ((Character.Equals('a')) || (Character.Equals('e')) || (Character.Equals('i')) || (Character.Equals('o')) || (Character.Equals('u')) || (Character.Equals('l')) || (Character.Equals('n')) || (Character.Equals('r')) || (Character.Equals('s')) || (Character.Equals('t')))
-          {
-            UserPoints += 1;
-          }
-          else if ((Character.Equals('d')) || (Character.Equals('g')))
-          {
-            UserPoints += 2;
-          }
-          else if ((Character.Equals('b')) || (Character.Equals('c')) || (Character.Equals('m')) || (Character.Equals('p'))) 
-          {
-            UserPoints += 3;
-          }
-          else if ((Character.Equals('f')) || (Character.Equals('h')) || (Character.Equals('v')) || (Character.Equals('w')) || (Character.Equals('y')))
-          {
-            UserPoints += 4;
-          }
-          else if ((Character.Equals('k')))
-          {
-            UserPoints += 5;
-          }
-          else if ((Character.Equals('j')) || (Character.Equals('x')))
-          {
-            UserPoints += 8;
-          }
-          else if ((Character.Equals('q')) || (Character.Equals('z')))
-          {
-            UserPoints += 10;
-          }
+      {                 
+        if ((Character.Equals('d')) || (Character.Equals('g')))
+        {
+          UserPoints += 2;
+        }
+        else if ((Character.Equals('b')) || (Character.Equals('c')) || (Character.Equals('m')) || (Character.Equals('p'))) 
+        {
+          UserPoints += 3;
+        }
+        else if ((Character.Equals('f')) || (Character.Equals('h')) || (Character.Equals('v')) || (Character.Equals('w')) || (Character.Equals('y')))
+        {
+          UserPoints += 4;
+        }
+        else if ((Character.Equals('k')))
+        {
+          UserPoints += 5;
+        }
+        else if ((Character.Equals('j')) || (Character.Equals('x')))
+        {
+          UserPoints += 8;
+        }
+        else if ((Character.Equals('q')) || (Character.Equals('z')))
+        {
+          UserPoints += 10;
+        }
+        else
+        {
+          UserPoints += 1;
+        }
       }
       return UserPoints;
     }
